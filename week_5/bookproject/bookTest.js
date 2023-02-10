77a829242fa23accfbb4bda6c4e4187554e34968
Chapagai Santosh <s2300121@BC-C02J63E0Q6LR-MacBook-Air.local>
2023-02-10 13:50:48 +0200

    creating library and booksAPI

--- week_5/bookproject/bookTest.js
+++ week_5/bookproject/bookTest.js
@@ -60,3 +60,39 @@ for(const person of library){
     }
     console.log(`${name}: ${sumOfPages} pages, ${totalPrice} €.`)
 }
+
+console.log('#### Total number of pages and total price per person as well as grand total ###');
+
+
+    let sumPrice= 0;
+    let totalPages = 0;
+for(const person of library){
+    let sumOfPages = 0;
+    
+    let totalPrice= 0;
+    
+
+    const name = `${person.firstname} ${person.lastname}`;
+    for(const book of person.books){
+        sumOfPages+=book.pages; 
+        totalPrice+=book.price;    
+
+    }
+    
+    console.log(`${name}: ${sumOfPages} pages, ${totalPrice} €.`);
+    totalPages += sumOfPages;
+    sumPrice += totalPrice;
+}
+console.log(`Total pages: ${totalPages} Total price: ${sumPrice} €`);
+
+
+function totalPriceOfTheLibrary(){
+    let sum = 0;
+    for(const person of library){
+        for(const book of person.books){
+            sum += book.price;
+        }
+    }
+    return sum;
+}
+console.log('The value of the library is' ,totalPriceOfTheLibrary(),'€');

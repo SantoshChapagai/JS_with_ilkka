0ac3497cf50076555272970207de650ef542e892
Chapagai Santosh <s2300121@BC-C02J63E0Q6LR-MacBook-Air.local>
2023-02-10 12:45:39 +0200

    creating library

---
+++ week_5/bookproject/bookTest.js
@@ -0,0 +1,62 @@
+'use strict'
+const library = require('./books.json');
+console.log(library[0].books);
+console.log(library[0].books[0]);
+console.log(library[0].books[0].name);
+console.log('#### first person ###');
+for (const book of library[0].books){
+    console.log(`${book.name}, ${book.price} €`);
+}
+console.log('#### second person ###');
+for (const book of library[1].books){
+    console.log(`${book.name}, ${book.price} €`);
+}
+
+console.log('#### all persons, all books #####');
+
+for(const person of library){
+    for (const book of person.books){
+        console.log(book.name);
+    }
+}
+console.log('##### version 2 #####');
+for(const person of library){
+    console.log(`${person.firstname} ${person.lastname}`);
+    for (const book of person.books){
+        console.log('\t' + book.name);
+    }
+}
+console.log('##### another version ####');
+for (const person of library){
+    const name = `${person.firstname} ${person.lastname}`;
+    for (const book of person.books){
+        console.log(`${name}: ${book.name}`);
+    }
+}
+
+console.log('### total number of pages per person ####');
+
+for(const person of library){
+    let sumOfPages = 0;
+    const name = `${person.firstname} ${person.lastname}`;
+    for(const book of person.books){
+        sumOfPages+=book.pages;
+        
+
+    }
+    console.log(`${name}: ${sumOfPages} pages`)
+}
+
+console.log('###total number of pages and total price per person ###');
+
+for(const person of library){
+    let sumOfPages = 0;
+    let totalPrice= 0;
+    const name = `${person.firstname} ${person.lastname}`;
+    for(const book of person.books){
+        sumOfPages+=book.pages; 
+        totalPrice+=book.price;    
+
+    }
+    console.log(`${name}: ${sumOfPages} pages, ${totalPrice} €.`)
+}
